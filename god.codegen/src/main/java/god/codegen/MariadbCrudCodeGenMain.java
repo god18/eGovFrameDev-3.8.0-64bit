f89ba17487d43696a4593345339fb7b82ac5e61e
이백행 <dlqorgod@naver.com>
2019-04-15 17:59:18 +0900

    SqlInsert

--- god.codegen/src/main/java/god/codegen/MariadbCrudCodeGenMain.java
+++ god.codegen/src/main/java/god/codegen/MariadbCrudCodeGenMain.java
@@ -3,7 +3,8 @@ package god.codegen;
 public class MariadbCrudCodeGenMain {
 
 	public static void main(String[] args) {
-		MariadbCrudCodeGen mariadbCrudCodeGen = new MariadbCrudCodeGen();
+		// MariadbCrudCodeGen mariadbCrudCodeGen = new MariadbCrudCodeGen();
+		CrudCodeGen mariadbCrudCodeGen = new MariadbCrudCodeGen();
 		mariadbCrudCodeGen
 				.setUp("C:\\Users\\godsoft\\Google 드라이브(godsoft18@gmail.com)\\codegen\\mariadb-10.1.37-winx64.xlsx");
 		mariadbCrudCodeGen.sql();
--- god.codegen/src/main/java/god/codegen/MariadbSql.java
+++ god.codegen/src/main/java/god/codegen/MariadbSql.java
@@ -1,11 +1,18 @@
 package god.codegen;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.SystemUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import egovframework.dev.imp.codegen.template.model.DataModelContext;
+import god.codegen.v2.SqlInsert;
 
 public class MariadbSql implements Sql {
 
@@ -23,7 +30,29 @@ public class MariadbSql implements Sql {
 			logger.debug("getName: {}", dataModel.getTables().getName());
 			logger.debug("getTableComment: {}", dataModel.getTables().getTableComment());
 			logger.debug("getAttributes: {}", dataModel.getAttributes());
+
+			String data = SqlInsert.insert(dataModel);
+			String pathname = SystemUtils.USER_HOME + "/Desktop/god/" + dataModel.getTables().getTableSchema() + "/"
+					+ dataModel.getTables().getTableName() + "/insert " + dataModel.getTables().getTableName() + ".sql";
+			generate(dataModel, data, pathname);
+
 			i++;
+		}
+	}
+
+	private void generate(DataModelContext dataModel, String data, String pathname) {
+		File file = new File(pathname);
+		// String data = null;
+		Charset encoding = StandardCharsets.UTF_8;
+		writeStringToFile(file, data, encoding);
+	}
+
+	private void writeStringToFile(File file, String data, Charset encoding) {
+		System.out.println("file: " + file);
+		try {
+			FileUtils.writeStringToFile(file, data, encoding);
+		} catch (IOException e) {
+			e.getMessage();
 		}
 	}
 
